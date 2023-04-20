6d07f0c2b8f03e43e9497c3c155884d20ff8e5ac
Jacob <jacobvestergaard@hotmail.dk>
2023-04-20 09:01:29 +0200

    hihi

--- Worker.cs
+++ Worker.cs
@@ -2,7 +2,6 @@ namespace PlanningService;
 using System.Text;
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
-
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
@@ -12,86 +11,180 @@ public class Worker : BackgroundService
     private IModel channel;
 
     private string csvPath = string.Empty;
+    private string csvPathService = string.Empty;
+    private string csvPathRepair = string.Empty;
+    private string[] routeWays = new string[0];
     public Worker(ILogger<Worker> logger, IConfiguration configuration)
     {
         _logger = logger;
 
-
+        string gg = configuration["routeWays"] ?? string.Empty;
+        routeWays = gg.Length > 0 ? gg.Split(",") : new string[0];
         string connectionString = configuration["RabbitMQConnectionString"] ?? string.Empty;
 
         csvPath = configuration["csvpath"] ?? string.Empty;
 
+        csvPathService = configuration["csvPathService"] ?? string.Empty;
+        csvPathRepair = configuration["csvPathRepair"] ?? string.Empty;
+
         factory = new ConnectionFactory() { HostName = connectionString };
         connection = factory.CreateConnection();
         channel = connection.CreateModel();
-        logger.LogInformation("WW");
+        logger.LogInformation("WW " + routeWays[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
 
+        
+           var queueName = channel.QueueDeclare().QueueName;
+            foreach (var bindingKey in routeWays)
+            {
+                channel.QueueBind(queue: queueName,
+                                exchange: "topic_logs",
+                                routingKey: bindingKey);
+            }
 
-        channel.QueueDeclare(queue: "hello",
-                 durable: false,
-                 exclusive: false,
-                 autoDelete: false,
-                 arguments: null);
+             _logger.LogInformation(routeWays.Length.ToString());
 
-
-        var consumer = new EventingBasicConsumer(channel);
-        consumer.Received += (model, ea) =>
-        {
-            try
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
             {
                 var body = ea.Body.ToArray();
                 var message = Encoding.UTF8.GetString(body);
-                _logger.LogInformation("Getting Json: " + message);
-
-                string csvFormat = "";
-                bool addCharFlag = false;
-                for (int i = 0; i < message.Length; i++)
-                {
-                    if (message[i] == ':' && message[i - 1] == '"')
-                    {
-                        addCharFlag = true;
-                        i++;
-                    }
-                    if (message[i] == ',')
-                    {
-
-                        addCharFlag = false;
-                        csvFormat += ',';
-                    }
-                    if (addCharFlag && message[i] != '"' && message[i] != '}' && message[i] != '{')
-                    {
-                        //Hejsa
-                        csvFormat += message[i];
-                    }
-                }
-
-                _logger.LogInformation("Sending: " + csvFormat);
-                File.AppendAllText(csvPath, csvFormat + Environment.NewLine);
-
-            }
-            catch (System.Exception ex)
-            {
-
-                _logger.LogError(ex.Message);
-            }
-        };
+                var routingKey = ea.RoutingKey;
+                _logger.LogInformation($" [x] Received '{routingKey}':'{message}'");
+            };
+            channel.BasicConsume(queue: queueName,
+                                autoAck: true,
+                                consumer: consumer);       
+        
 
 
-        channel.BasicConsume(queue: "hello",
-                 autoAck: true,
-                 consumer: consumer);
 
 
         while (!stoppingToken.IsCancellationRequested)
         {
-            _logger.LogInformation("Nyt data: " + stoppingToken.ToString());
+            _logger.LogInformation("Nyt data: ");
 
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
             await Task.Delay(10000, stoppingToken);
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /*
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+
+
+            channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                try
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    _logger.LogInformation("Getting Json: " + message);
+
+                    string csvFormat = "";
+                    bool addCharFlag = false;
+                    for (int i = 0; i < message.Length; i++)
+                    {
+                        if (message[i] == ':' && message[i - 1] == '"')
+                        {
+                            addCharFlag = true;
+                            i++;
+                        }
+                        if (message[i] == ',')
+                        {
+
+                            addCharFlag = false;
+                            csvFormat += ',';
+                        }
+                        if (addCharFlag && message[i] != '"' && message[i] != '}' && message[i] != '{')
+                        {
+                            //Hejsa
+                            csvFormat += message[i];
+                        }
+                    }
+
+                    _logger.LogInformation("Sending: " + csvFormat);
+                    File.AppendAllText(csvPath, csvFormat + Environment.NewLine);
+
+                }
+                catch (System.Exception ex)
+                {
+
+                    _logger.LogError(ex.Message);
+                }
+            };
+
+
+            channel.BasicConsume(queue: "hello",
+                     autoAck: true,
+                     consumer: consumer);
+
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Nyt data: " + stoppingToken.ToString());
+
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(10000, stoppingToken);
+            }
+        } */
 }
